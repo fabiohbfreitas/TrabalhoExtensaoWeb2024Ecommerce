571e0a838348ddb1c17d2f2598ebb62d54379c7e
fabiohbfreitas <difreitas.fabio@gmail.com>
2024-06-20 12:26:41 -0300

    add orders and reviews repository

--- src/lib/database.mjs
+++ src/lib/database.mjs
@@ -31,6 +31,46 @@ try {
             on delete cascade
             on update cascade
     );
+
+    create table if not exists review (
+        id integer primary key autoincrement,
+        productId text not null,
+        review text not null,
+
+        foreign key (productId)
+            references product (id)
+            on delete cascade
+            on update cascade
+    );
+    create table if not exists checkout_order (
+        id integer primary key autoincrement,
+        date text not null,
+        customerId text not null,
+        approved bool not null,
+
+        foreign key (customerId)
+            references user (id)
+            on delete cascade
+            on update cascade
+    );
+
+    create table if not exists order_product (
+        id integer primary key autoincrement,
+        orderId integer not null,
+        productId text not null,
+        quantity integer not null,
+        subtotal integer not null, 
+
+        foreign key (orderId)
+            references checkoutOrder (id)
+            on delete cascade
+            on update cascade
+        
+        foreign key (productId)
+            references product (id)
+            on delete cascade
+            on update cascade
+    );
     `)
 
     // let catInsert = db.prepare(`
@@ -70,15 +110,23 @@ try {
     // db.exec("delete from user;")
     // let res = db.prepare("SELECT * from category limit 100;").all()
     // console.log(res);
-
+    // db.prepare(`
+    //     insert into category (category) values (?);
+    // `).run("cat1")
     // let insertProduct = db.prepare(`
     //     insert into product values (?, ?, ?, ?, ?, ?);
-    // `).run("asdf", "title", "description", 10, 69, 1);
+    // `).run("asdf", "title", "description", 10, 69, 3);
     // console.log(insertProduct);
 
     // let deleteProduct = db.prepare("delete from product where id == ?;").run("randomid453")
     // console.log(deleteProduct);
 
+    // let approveOrder  = db.prepare(`
+    //     update checkout_order
+    //         set approved = ?
+    //     where
+    //         id == ?
+    // `).run(true, id)
     // let updateProduct = db.prepare(`
     //     update product
     //         set title = ?,
@@ -90,9 +138,17 @@ try {
     // `).run("updated title", "updated desc", 99, 420, "asdf");
     // console.log(updateProduct);
 
-    // console.log(db.prepare("select * from product where id = ? limit 1;").get("asdf"))
+    // console.log(db.prepare(`
+    //     insert into review (productID, review) values (?, ?);
+    // `).run("asdf", "muito pica"));
 
-    // db.exec("delete from category;")
-} catch (er) {
-    console.error(er.message)
+    // let insertReview = db.prepare(`
+    //     delete from review where id = ?;
+    // `).run(1);
+    // console.log(insertReview);
+
+    console.log(db.prepare("select * from review where productId = ? limit 100;").all("asdf"))
+
+} catch (error) {
+    console.error(error.message)
 }
